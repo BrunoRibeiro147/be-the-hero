51e3ce4b80a93f3c8938a5f88c56cda7ee788073
Bruno Ribeiro <brf147@gmail.com>
2020-04-27 18:37:00 -0400

    Add Images Repository

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,66 +1,60 @@
 const connection = require('../database/connection');
 
-
 module.exports = {
+  async index(req, res) {
+    const { page = 1 } = req.query;
 
-    async index(req,res){
-        const {page = 1} = req.query; 
+    const [count] = await connection('incidents').count();
 
-        const [count] = await connection('incidents')
-            .count();
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf',
+      ]);
 
-        console.log(count);
+    res.header('X-Total-Count', count['count(*)']);
 
-        const incidents = await connection('incidents')
-            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select([
-                'incidents.*',
-                'ongs.name', 
-                'ongs.email', 
-                'ongs.whatsapp', 
-                'ongs.city', 
-                'ongs.uf']);
+    return res.json(incidents);
+  },
 
-        res.header('X-Total-Count',count['count(*)']);
+  async store(req, res) {
+    const { title, description, value } = req.body;
 
-        return res.json(incidents);
-    },
+    const ong_id = req.headers.authorization;
 
-    async store(req,res) {
-        const {title,description,value} = req.body;
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id,
+    });
 
-        const ong_id = req.headers.authorization;
+    return res.json({ id });
+  },
 
-        const [id] = await connection('incidents').insert({
-            title,
-            description,
-            value,
-            ong_id
-        });
+  async delete(req, res) {
+    const { id } = req.params;
 
-        return res.json({id});
-    },
+    const ong_id = req.headers.authorization;
 
-    async delete(req,res) {
-        const { id } = req.params;
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
 
-        const ong_id = req.headers.authorization;
-
-        const incident = await connection('incidents')
-            .where('id',id)
-            .select('ong_id')
-            .first();
-
-        if (incident.ong_id !== ong_id) {
-            return res.status(401).json({error : "Operation Not Permitted."})
-        }
-
-        await connection('incidents').where("id",id).delete();
-
-        return res.status(204).send();
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: 'Operation Not Permitted.' });
     }
 
+    await connection('incidents').where('id', id).delete();
 
-}
+    return res.status(204).send();
+  },
+};
